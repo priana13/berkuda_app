20556e48ecdf2356e41183a4f7b4eb5a62b20bc4
priana <herian.sap@gmail.com>
2021-10-19 23:20:39 +0700

    tambah modul post dan category

--- app/Http/Controllers/AdminCmsUsersController.php
+++ app/Http/Controllers/AdminCmsUsersController.php
@@ -22,16 +22,22 @@ class AdminCmsUsersController extends CBController {
 		# START COLUMNS DO NOT REMOVE THIS LINE
 		$this->col = array();
 		$this->col[] = array("label"=>"Name","name"=>"name");
+		$this->col[] = array("label"=>"HP","name"=>"hp");		
 		$this->col[] = array("label"=>"Email","name"=>"email");
 		$this->col[] = array("label"=>"Privilege","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
-		$this->col[] = array("label"=>"Photo","name"=>"photo","image"=>1);		
+		$this->col[] = array("label"=>"Status","name"=>"status_member");
+
 		# END COLUMNS DO NOT REMOVE THIS LINE
 
 		# START FORM DO NOT REMOVE THIS LINE
 		$this->form = array(); 		
 		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
-		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000','resize_width'=>90,'resize_height'=>90);											
+		$this->form[] = array("label"=>"HP","name"=>"hp",'validation'=>'min:3');
+		$this->form[] = array("label"=>"Kota","name"=>"kota",'validation'=>'min:3');
+		$this->form[] = array("label"=>"Alamat","name"=>"alamat",'validation'=>'min:3');
+		$this->form[] = array("label"=>"Email","name"=>"email",'type'=>'email','validation'=>'email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
+		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'validation'=>'image|max:1000','resize_width'=>90,'resize_height'=>90);											
+		$this->form[] = array("label"=>"Status Member","name"=>"status_member", 'type' => 'select2' ,'validation'=>'min:3','dataenum' => 'Trial;Member;Internal');
 		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);						
 		// $this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
 		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
